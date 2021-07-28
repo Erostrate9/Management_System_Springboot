fe045dc64aa079543f5617c6f60275c8d09781f9
eros9 <xxuanf0934@gmail.com>
2021-07-28 09:34:29 +0800

    hello springboot

---
+++ src/main/java/com/example/demo3/controller/AccountController.java
@@ -0,0 +1,14 @@
+package com.example.demo3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("account")
+public class AccountController {
+    @GetMapping("toLogin")
+    public String toLogin(){
+        return "Login";
+    }
+}
