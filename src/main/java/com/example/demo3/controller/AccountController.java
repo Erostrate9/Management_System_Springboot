260d60801f0830fbc105afbba5d5b8c1bf7f0da0
eros9 <xxuanf0934@gmail.com>
2021-07-28 15:35:28 +0800

    finish mybatis demo, before hibernate

--- src/main/java/com/example/demo3/controller/AccountController.java
+++ src/main/java/com/example/demo3/controller/AccountController.java
@@ -4,6 +4,7 @@ import com.example.demo3.entity.Account;
 import com.example.demo3.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,10 +32,10 @@ public class AccountController {
     public String tomain(){return "main";}
 
     @GetMapping("toChuzhang")
-    public String toChuzhang(){
+    public String toChuzhang(Model model){
         //获取集合数据
         List<Account> list=asi.getAccount();
-
+        model.addAttribute("accountList",list);
         return "chuzhang";
     }
 }
